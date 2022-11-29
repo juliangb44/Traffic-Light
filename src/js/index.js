ca94934bea59f8c9ec84b23b264b8732b5bd7fdf
juliangb44 <juliangb44@gmail.com>
2022-11-29 18:44:06 +0000

    last update

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+
+//include your index.scss file into the bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import TrafficLight  from "./component/TrafficLight";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
